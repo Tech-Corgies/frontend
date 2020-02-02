12102a004684a63ab84d1ec6b337f7ad94bf8b88
isabiiil <40670279+isabiiil@users.noreply.github.com>
2020-02-02 00:12:31 -0500

    Created home page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,84 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import useActions from './utils/useActions';
-import * as a from './modules/user/userAction';
+import styled, { css } from 'styled-components';
+import './index.css';
 
 function App() {
-  const [googleAuth, login, logout, register] = useActions([
-    a.googleAuthorized,
-    a.userLogin,
-    a.userLogout,
-    a.userRegister,
-  ]);
-  useEffect(() => {
-    login({ email: 'tester1121@email.com', password: 'securePassw2ord111' });
-  }, []);
+  const Title = styled.h1`
+  text-align: center;
+  font-size: 90px;
+  line-height: 0px;
+  // color: mediumseagreen;
+  `;
+  const Subtitle = styled.h1`
+  font-size: 50px;
+  line-height: 5px;
+  text-align: center;
+  position: relative; 
+  bottom: 55px;
+  // color: mediumseagreen;
+  `;
+  const Wrapper = styled.section`
+  display:flex;
+  flex-direction: column;
+  padding: 0px 20px 0px 20px;
+  height: 100vh;
+  `;
+  const NavBar = styled.section`
+  align-self: flex-end;
+  display: inline;
+  `;
+  const Bar = styled.section`
+  align-self: center;
+  `;
+  const Button = styled.button`
+  color: mediumseagreen;
+  align-self: center;
+  font-size: 18px;
+  background: mediumseagreen;
+  border-radius: 3px;
+  margin: 10px;
+  padding: 10px;
+  border: none;
+  color: white;
+  max-width: 50vw;
+  `;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hey</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Wrapper>
+      <NavBar>
+      <Button>
+          <a href = "./App.js">Home</a>
+        </Button>
+        <Button>
+          <a href = "./About.js">About</a>
+        </Button>
+        <Button>
+          <a href = "./Team.js">Team</a>
+        </Button>
+      </NavBar>
+      <Title>
+        <img className = "e" src = 'https://www.shareicon.net/data/512x512/2015/10/16/657287_logo_512x512.png' />
+        nviro:
+      </Title>
+      <Subtitle>
+        Conveniently Carbon Free
+      </Subtitle>
+      <Bar>
+      <Button>
+      <a href = "./Signup.js">Sign Up</a>
+      </Button>
+      <Button>
+        <a href = "./Login.js">Log In</a>
+      </Button>
+      </Bar>
+      <Bar>
+        <Button style={{width: "15vw"}}>
+          <a href = "Camera.js">Try Me</a>
+        </Button>
+      </Bar>
+    </Wrapper>
+  </div>
   );
 }
 
